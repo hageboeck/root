088436bf4453c3e21746d0f5e6e5634fca02bca6
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2018-10-23 13:26:40 +0200

    Fix uninit pointer in RooFFTConvPdf.

--- roofit/roofitcore/inc/RooAddPdf.h
+++ roofit/roofitcore/inc/RooAddPdf.h
@@ -132,7 +132,7 @@ protected:
 
   RooListProxy _pdfList ;   //  List of component PDFs
   RooListProxy _coefList ;  //  List of coefficients
-  mutable RooArgList* _snormList ;  //!  List of supplemental normalization factors
+  mutable RooArgList* _snormList{nullptr} ;  //!  List of supplemental normalization factors
   TIterator* _pdfIter ;     //! Iterator over PDF list
   TIterator* _coefIter ;    //! Iterator over coefficient list
   
