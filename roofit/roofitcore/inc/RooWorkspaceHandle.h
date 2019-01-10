aa679f0f68ef1554cb9353529d0ca11cba325c8b
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2019-01-10 11:50:25 +0100

    Cleanups for RooWorkspaceHandle implementation.
    - Format comments
    - Increase ModelConfig class version
    - Remove prints in RooWorkspace UTs and create test fixture.

--- roofit/roofitcore/inc/RooWorkspaceHandle.h
+++ roofit/roofitcore/inc/RooWorkspaceHandle.h
@@ -1,18 +1,26 @@
 // Author: Stephan Hageboeck, CERN  01/2019
 
-/*************************************************************************
- * Copyright (C) 1995-2019, Rene Brun and Fons Rademakers.               *
- * All rights reserved.                                                  *
- *                                                                       *
- * For the licensing terms see $ROOTSYS/LICENSE.                         *
- * For the list of contributors see $ROOTSYS/README/CREDITS.             *
- *************************************************************************/
+/*****************************************************************************
+ * RooFit
+ * Authors:                                                                  *
+ *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
+ *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
+ *                                                                           *
+ * Copyright (c) 2000-2019, Regents of the University of California          *
+ *                          and Stanford University. All rights reserved.    *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,                        *
+ * with or without modification, are permitted according to the terms        *
+ * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
+ *****************************************************************************/
+
 
 #ifndef ROOFIT_ROOFITCORE_INC_ROOWORKSPACEHANDLE_H_
 #define ROOFIT_ROOFITCORE_INC_ROOWORKSPACEHANDLE_H_
 
 #include "RooWorkspace.h"
 
+///////////////////////////////////////////////////////////////////////////////////////////////
 /// An interface to set and retrieve a workspace.
 /// This is needed for all generic objects that can be saved in a workspace, which itself depend
 /// on the workspace (e.g. the RooStats::ModelConfig).
@@ -20,7 +28,7 @@
 /// The handle hides this dependency.
 class RooWorkspaceHandle {
 public:
-   virtual ~RooWorkspaceHandle() {}
+   virtual ~RooWorkspaceHandle() = default;
 
    ///Set the workspace. If it exists, it is up to the implementing class to decide how to proceed.
    virtual void SetWS(RooWorkspace &ws) = 0;
--- roofit/roofitcore/test/testWorkspace.cxx
+++ roofit/roofitcore/test/testWorkspace.cxx
@@ -1,10 +1,10 @@
-/// Tests for the RooWorkspace
+// Tests for the RooWorkspace
+// Authors: Stephan Hageboeck, CERN  01/2019
 
+#include "RooWorkspace.h"
+#include "RooRealVar.h"
 #include "RooGlobalFunc.h"
 #include "RooGaussian.h"
-#include "RooArgList.h"
-#include "RooRealVar.h"
-#include "RooAbsReal.h"
 #include "RooStats/ModelConfig.h"
 
 #include "TFile.h"
@@ -12,59 +12,58 @@
 
 #include "gtest/gtest.h"
 
-using namespace RooStats;
+class TestRooWorkspaceWithGaussian : public ::testing::Test {
+protected:
+  TestRooWorkspaceWithGaussian() :
+  Test()
+  {
+    RooRealVar x("x", "x", 1, 0, 10);
+    RooRealVar mu("mu", "mu", 1, 0, 10);
+    RooRealVar sigma("sigma", "sigma", 1, 0, 10);
+
+    RooGaussian pdf("Gauss", "Gauss", x, mu, sigma);
+
+    TFile outfile(_filename, "RECREATE");
+
+    // now create the model config for this problem
+    RooWorkspace w("ws");
+    RooStats::ModelConfig modelConfig("ModelConfig", &w);
+    modelConfig.SetPdf(pdf);
+    modelConfig.SetParametersOfInterest(RooArgSet(sigma));
+    modelConfig.SetGlobalObservables(RooArgSet(mu));
+    w.import(modelConfig);
+
+    outfile.WriteObject(&w, "ws");
+  }
+
+  ~TestRooWorkspaceWithGaussian() {
+    gSystem->Unlink(_filename);
+  }
+
+  const char* _filename = "ROOT-9777.root";
+};
+
 
 /// ROOT-9777, cloning a RooWorkspace. The ModelConfig did not get updated
 /// when a workspace was cloned, and was hence pointing to a non-existing workspace.
 ///
-TEST(RooWorkspace, CloneModelConfig_ROOT_9777)
+TEST_F(TestRooWorkspaceWithGaussian, CloneModelConfig_ROOT_9777)
 {
-   const char* filename = "ROOT-9777.root";
+  std::unique_ptr<RooWorkspace> w2;
+  {
+    TFile infile(_filename, "READ");
+    RooWorkspace *w;
+    infile.GetObject("ws", w);
+    ASSERT_TRUE(w) << "Workspace not read from file.";
 
-   RooRealVar x("x", "x", 1, 0, 10);
-   RooRealVar mu("mu", "mu", 1, 0, 10);
-   RooRealVar sigma("sigma", "sigma", 1, 0, 10);
+    w2 = std::make_unique<RooWorkspace>(*w);
+    delete w;
+  }
 
-   RooGaussian pdf("Gauss", "Gauss", x, mu, sigma);
+  RooStats::ModelConfig *mc = dynamic_cast<RooStats::ModelConfig*>(w2->genobj("ModelConfig"));
 
-   {
-      TFile outfile(filename, "RECREATE");
-      
-      // now create the model config for this problem
-      RooWorkspace* w = new RooWorkspace("ws");
-      ModelConfig modelConfig("ModelConfig", w);
-      modelConfig.SetPdf(pdf);
-      modelConfig.SetParametersOfInterest(RooArgSet(sigma));
-      modelConfig.SetGlobalObservables(RooArgSet(mu));
-      w->import(modelConfig);
-
-      outfile.WriteObject(w, "ws");
-      delete w;
-   }
-   
-   RooWorkspace *w2;
-   {
-      TFile infile(filename, "READ");
-      RooWorkspace *w;
-      infile.GetObject("ws", w);
-      ASSERT_TRUE(w) << "Workspace not read from file.";
-
-      w2 = new RooWorkspace(*w);
-      delete w;
-   }
-   
-   w2->Print();
-
-   ModelConfig *mc = dynamic_cast<ModelConfig*>(w2->genobj("ModelConfig"));
-   ASSERT_TRUE(mc) << "ModelConfig not retrieved.";
-   mc->Print();
-
-   ASSERT_TRUE(mc->GetGlobalObservables()) << "GlobalObsevables in mc broken.";
-   mc->GetGlobalObservables()->Print();
-
-   ASSERT_TRUE(mc->GetParametersOfInterest()) << "ParametersOfInterest in mc broken.";
-   mc->GetParametersOfInterest()->Print();
-
-   gSystem->Unlink(filename);
+  EXPECT_TRUE(mc) << "ModelConfig not retrieved.";
+  EXPECT_TRUE(mc->GetGlobalObservables()) << "GlobalObservables in ModelConfig broken.";
+  EXPECT_TRUE(mc->GetParametersOfInterest()) << "ParametersOfInterest in ModelConfig broken.";
 }
 
