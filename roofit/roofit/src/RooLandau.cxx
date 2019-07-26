2be0476046b607898be59ffc36f80a0ad16cd834
Emmanouil Michalainas <manolis@pcepsft100.dyndns.cern.ch>
2019-07-26 09:49:44 +0200

    [RF] Vectorised Landau PDF

--- roofit/roofit/src/RooLandau.cxx
+++ roofit/roofit/src/RooLandau.cxx
@@ -21,16 +21,20 @@ Landau distribution p.d.f
 \image html RF_Landau.png "PDF of the Landau distribution."
 **/
 
+#include "RooLandau.h"
 #include "TMath.h"
 #include "RooFit.h"
 
-#include "RooLandau.h"
-#include "RooLandau.h"
 #include "RooRandom.h"
+#include "BatchHelpers.h"
 
 #include "TError.h"
 
+#include "LandauBatchEvaluate.h" // for compute()
+
 using namespace std;
+using namespace BatchHelpers;
+using namespace LandauBatchEvaluate;
 
 ClassImp(RooLandau);
 
@@ -60,6 +64,55 @@ Double_t RooLandau::evaluate() const
 {
   return TMath::Landau(x, mean, sigma);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/// Compute \f$ Landau(x,mean,sigma) \f$ in batches.
+/// The local proxies {x, mean, sigma} will be searched for batch input data,
+/// and if found, the computation will be batched over their
+/// values. If batch data are not found for one of the proxies, the proxies value is assumed to
+/// be constant over the batch.
+/// \param[in] batchIndex Index of the batch to be computed.
+/// \param[in] batchSize Size of each batch. The last batch may be smaller.
+/// \return A span with the computed values.
+
+RooSpan<double> RooLandau::evaluateBatch(std::size_t begin, std::size_t batchSize) const {
+  auto output = _batchData.makeWritableBatchUnInit(begin, batchSize);
+
+  auto xData = x.getValBatch(begin, batchSize);
+  auto meanData = mean.getValBatch(begin, batchSize);
+  auto sigmaData = sigma.getValBatch(begin, batchSize);
+  const bool batchX = !xData.empty();
+  const bool batchMean = !meanData.empty();
+  const bool batchSigma = !sigmaData.empty();
+  if (batchX && !batchMean && !batchSigma ) {
+    compute(output, xData, BracketAdapter<RooRealProxy>(mean), BracketAdapter<RooRealProxy>(sigma));
+  }
+  else if (!batchX && batchMean && !batchSigma ) {
+    compute(output, BracketAdapter<RooRealProxy>(x), meanData, BracketAdapter<RooRealProxy>(sigma));
+  }
+  else if (batchX && batchMean && !batchSigma ) {
+    compute(output, xData, meanData, BracketAdapter<RooRealProxy>(sigma));
+  }
+  else if (!batchX && !batchMean && batchSigma ) {
+    compute(output, BracketAdapter<RooRealProxy>(x), BracketAdapter<RooRealProxy>(mean), sigmaData);
+  }
+  else if (batchX && !batchMean && batchSigma ) {
+    compute(output, xData, BracketAdapter<RooRealProxy>(mean), sigmaData);
+  }
+  else if (!batchX && batchMean && batchSigma ) {
+    compute(output, BracketAdapter<RooRealProxy>(x), meanData, sigmaData);
+  }
+  else if (batchX && batchMean && batchSigma ) {
+    compute(output, xData, meanData, sigmaData);
+  }
+  else{
+    throw std::logic_error("Requested a batch computation, but no batch data available.");
+  }
+
+  return output;
+}
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 
