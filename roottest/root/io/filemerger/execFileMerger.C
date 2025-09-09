47f8c82635c6755b01784f14576a29c5ca6963b8
Stephan Hageboeck <stephan.hageboeck@cern.ch>
2025-09-09 11:32:33 +0200

    [NFC] Fix stray white spaces.

--- roottest/root/io/filemerger/execFileMerger.C
+++ roottest/root/io/filemerger/execFileMerger.C
@@ -23,7 +23,7 @@ void createInputs(int n = 2)
       Double_t coord[5] = {0.5, 1.5, 2.5, 3.5, 4.5};
       sparse->Fill(coord);
       sparse->Write();
-      
+
       THStack *stack = new THStack("stack","");
       h = new TH1F("hs_1","",10,0,100);
       h->Fill(10.5); h->Fill(20.5);
@@ -40,9 +40,9 @@ void createInputs(int n = 2)
       gr->SetPoint(0,1,1);
       gr->SetPoint(1,2,2);
       gr->SetPoint(2,3,3);
-      
+
       gr->Write();
-      
+
       TTree *tree = new TTree("tree","simplistic tree");
       Int_t data = 0;
       tree->Branch("data",&data);
@@ -50,7 +50,7 @@ void createInputs(int n = 2)
          data = l;
          tree->Fill();
       }
-      
+
       file->Write();
       delete file;
    }
@@ -83,7 +83,7 @@ bool check(int n = 2) {
       Error("execFileMerger","h1 not added properly");
       result = false;
    }
-   
+
    THnSparseF *sparse; file->GetObject("sparse",sparse);
    if (!sparse) {
       Error("execFileMerger","sparse is missing\n");
@@ -103,7 +103,7 @@ bool check(int n = 2) {
          result = false;
       }
    }
-   
+
    THStack *stack; file->GetObject("stack",stack);
    if (!stack) {
       Error("execFileMerger","stack is missing\n");
@@ -135,18 +135,18 @@ bool check(int n = 2) {
    }
    if (gr->GetN() != ( n * 3)) {
       Error("execFileMerger","exgraph not added properly n=%d rather than %d",gr->GetN(),n*3);
-      result = false;            
+      result = false;
    } else {
       for(Int_t k = 0; k < gr->GetN(); ++k) {
          double x,y;
          gr->GetPoint(k,x,y);
          if ( x != ( (k%3)+1 ) ||  y != ( (k%3)+1 ) ) {
             Error("execFileMerger","exgraph not added properly");
-            result = false;            
+            result = false;
          }
       }
    }
-   
+
    TTree *tree; file->GetObject("tree",tree);
    if (!tree) {
       Error("execFileMerger","tree is missing\n");
@@ -154,14 +154,14 @@ bool check(int n = 2) {
    }
    if (tree->GetEntries() != n*2) {
       Error("execFileMerger","tree does not have the expected number of entries: %lld rather than %d",tree->GetEntries(),n*2);
-      result = false;            
+      result = false;
    } else {
       if ( tree->GetEntries("data==1") != n ) {
          Error("execFileMerger","tree does not have the expected data. We got %lld entries with 'data==1' rather than %d",tree->GetEntries("data==1"),n);
          tree->Scan();
          result = false;
       }
-   }   
+   }
    return result;
 }
 
